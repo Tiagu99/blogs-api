9b26df652808a1546bae05094133aaacb31cdb57
Tiago Moreira <tiago.joke@yahoo.com.br>
2022-01-24 05:16:15 -0300

    corrige middlewate validateUpdate

--- middleware/blogpost/validateUpdate.js
+++ middleware/blogpost/validateUpdate.js
@@ -1,14 +1,14 @@
 module.exports = (req, res, next) => {
   const { title, content, categoryIds } = req.body;
   if (categoryIds) {
-    return res.status({ message: 'Categories cannot be edited' });
+    return res.status(400).send({ message: 'Categories cannot be edited' });
   }
   if (!title) {
-    return res.status({ message: '"title" is required' });
+    return res.status(400).send({ message: '"title" is required' });
   }
   if (!content) {
-    return res.status({ message: '"content" is required' });
+    return res.status(400).send({ message: '"content" is required' });
   }
-  
+
   return next();
 };
