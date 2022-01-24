868a2deabf04360ec3f097f0fc23e54d186a9fdb
Tiago Moreira <tiago.joke@yahoo.com.br>
2022-01-23 23:41:33 -0300

    implemeta root

---
+++ controller/root.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const root = express.Router({ mergeParams: true });
+
+root.use('/user', require('./user/router'));
+root.use('/login', require('./login/router'));
+
+module.exports = root;
